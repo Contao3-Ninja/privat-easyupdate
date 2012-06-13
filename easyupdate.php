e0cecc8db0593ff688a855d59d7b96d8545bedea
Glen Langer <contao.modul@glen-langer.de>
2012-06-13 22:09:03 +0200

    txt auf md bei Changelog Dateiname

--- easyupdate.php
+++ easyupdate.php
@@ -19,16 +19,20 @@ if (!defined('TL_ROOT'))
  * License along with this program. If not, please visit the Free
  * Software Foundation website at http://www.gnu.org/licenses/.
  * PHP version 5
+ * 
  * @copyright	Copyright easyupdate 2009
  * @author		Lutz Schoening
  * @package		easyupdate
  * @license		LGPL
  */
-class easyupdate extends BackendModule {
+class easyupdate extends BackendModule 
+{
 	// template var
 	protected $strTemplate = 'be_easyupdate';
 	protected $IMPORT;
-	protected function compile() {
+	
+	protected function compile() 
+	{
 		ini_set("memory_limit", "64M");
 		$archive = $this->Input->GET('filename');
 		$task = ($archive == 'n.a.' ? 0 : $this->Input->GET('task'));
@@ -36,7 +40,8 @@ class easyupdate extends BackendModule {
 		$this->Template->referer = $this->getReferer(ENCODE_AMPERSANDS);
 		$this->Template->backTitle = specialchars($GLOBALS['TL_LANG']['easyupdate']['backBT']);
 		$this->Template->headline = sprintf($GLOBALS['TL_LANG']['easyupdate']['headline'], VERSION . '.' . BUILD);
-		switch ($task) {
+		switch ($task) 
+		{
 			case 1 :
 				$this->Template->ModuleList = $this->showInformation($archive, $config_post);
 				break;
@@ -57,13 +62,22 @@ class easyupdate extends BackendModule {
 				break;
 		}
 	}
-	// get the file select box and the readme list
-	protected function getFiles() {
+	
+	
+    /**
+	 * get the file select box and the readme list
+	 * @return string
+	 */
+	protected function getFiles() 
+	{
 		$real_path = TL_ROOT . '/tl_files/easyupdate';
-		if (is_dir($real_path)) {
-			foreach (scandir($real_path, TRUE) as $file) {
+		if (is_dir($real_path)) 
+		{
+			foreach (scandir($real_path, TRUE) as $file) 
+			{
 				// remove hidden files and add only the zip files
-				if ($file[0] != '.' && substr($file, -3) == 'zip' && substr($file, 0, 3) != 'bak') {
+				if ($file[0] != '.' && substr($file, -3) == 'zip' && substr($file, 0, 3) != 'bak') 
+				{
 					$strAllFiles .= sprintf('<option value="%s">%s</option>', $file, $file);
 				}
 			}
@@ -71,13 +85,16 @@ class easyupdate extends BackendModule {
 		if (!$strAllFiles)
 			$strAllFiles .= sprintf('<option value="%s">%s</option>', 'n.a.', 'n.a.');
 		$real_path = $real_path . '/backup';
-		if (is_dir($real_path)) {
-			foreach (scandir($real_path, TRUE) as $file) {
+		if (is_dir($real_path)) 
+		{
+			foreach (scandir($real_path, TRUE) as $file) 
+			{
 				// remove hidden files and add only the zip files
 				if ($file[0] != '.' && substr($file, -3) == 'zip' && substr($file, 0, 3) == 'bak')
 					$strAllBackups .= sprintf('<option value="%s">%s</option>', $file, $file);
 			}
-			if ($strAllBackups) {
+			if ($strAllBackups) 
+			{
 				$strAllFiles = '<optgroup label=" ' . $GLOBALS['TL_LANG']['easyupdate']['files']['original'] . '">' . $strAllFiles . '</optgroup>';
 				$strAllBackups = '<optgroup label=" ' . $GLOBALS['TL_LANG']['easyupdate']['files']['backup'] . '">' . $strAllBackups . '</optgroup>';
 			}
@@ -101,21 +118,33 @@ class easyupdate extends BackendModule {
 		$return .= '</div></div>';
 		return $return;
 	}
-	// get the version number and compare it
-	protected function showInformation($archive, $config_post) {
+	
+	/**
+	 * get the version number and compare it
+	 * @param string $archive
+	 * @param array $config_post
+	 * @return string
+	 */
+	protected function showInformation($archive, $config_post) 
+	{
 		$archive = 'tl_files/easyupdate/' . (substr($archive, 0, 3) == 'bak' ? 'backup/' : '') . $archive;
 		$config = $config_post ? $config_post['files'] : unserialize($GLOBALS['TL_CONFIG']['easyUpdate']);
-		if ($config_post) {
+		if ($config_post) 
+		{
 			$update = $config_post['update'];
 			$this->IMPORT = unserialize($config_post['import']);
-		} else {
+		} 
+		else 
+		{
 			$objArchive = new ZipReaderTL($archive);
 			$arrFiles = $objArchive->getFileList();
 			$i = strpos($arrFiles[0], '/') + 1;
 			array_shift($arrFiles);
-			while ($objArchive->next()) {
+			while ($objArchive->next()) 
+			{
 				$strfile = substr($objArchive->file_name, $i);
-				if ($strfile == 'system/constants.php') {
+				if ($strfile == 'system/constants.php') 
+				{
 					$constants = ($objArchive->unzip());
 					break;
 				}
@@ -128,7 +157,8 @@ class easyupdate extends BackendModule {
 		$return .= '<div style="width:700px; margin:0 auto;">';
 		$return .= '<div style="float:right; width:60%;">';
 		$return .= '<h2 style="padding:0px 0px 0px 10px;">' . sprintf($GLOBALS['TL_LANG']['easyupdate']['updatex'], VERSION . '.' . BUILD, $this->IMPORT['VERSION'] . '.' . $this->IMPORT['BUILD']) . '</h2>';
-		switch ($update) {
+		switch ($update) 
+		{
 			case 1 :
 				$return .= '<div style="color:#8ab858; padding:0px 10px 0px 10px;">' . $GLOBALS['TL_LANG']['easyupdate']['update1'] . '</div>';
 				break;
@@ -141,11 +171,13 @@ class easyupdate extends BackendModule {
 		}
 		$real_path = $this->Environment->documentRoot . $this->Environment->path . '/system/config';
 		$strConfig .= "<b>&nbsp;" . $GLOBALS['TL_LANG']['easyupdate']['config_legend'] . "</b><br/>";
-		if (is_dir($real_path)) {
+		if (is_dir($real_path)) 
+		{
 			$intall = $intcheck = 0;
 			foreach (scandir($real_path) as $file) {
 				// remove hidden files and add only the zip files
-				if ($file[0] != '.' && substr($file, -3) == 'php') {
+				if ($file[0] != '.' && substr($file, -3) == 'php') 
+				{
 					$intall++;
 					$checked = $config[$file] == 1 ? checked : '';
 					$intcheck = $checked == checked ? ++ $intcheck : $intcheck;
@@ -155,7 +187,8 @@ class easyupdate extends BackendModule {
 		}
 		$strConfig .= "<b>&nbsp;" . $GLOBALS['TL_LANG']['easyupdate']['other_legend'] . "</b><br/>";
 		$file = "robots.txt";
-		if (is_file($this->Environment->documentRoot . $this->Environment->path . "/" . $file)) {
+		if (is_file($this->Environment->documentRoot . $this->Environment->path . "/" . $file)) 
+		{
 			$checked = $config[$file] == 1 ? checked : '';
 			$strConfig .= sprintf('<input type="checkbox" id="config" name="config[files][%s]" value="1" ' . $checked . ' onChange="document.tl_select_config.submit();"/>%s<br/>', $file, $file);
 		}
@@ -163,13 +196,16 @@ class easyupdate extends BackendModule {
 		$checked = $config[demo] == 1 ? checked : '';
 		$strConfig .= sprintf('<input type="checkbox" id="config" name="config[files][demo]" value="1" ' . $checked . ' onChange="document.tl_select_config.submit();"/>%s<br/>', $file);
 		// add the exclude files to the config
-		if ($GLOBALS['TL_CONFIG']['easyUpdate']) {
+		if ($GLOBALS['TL_CONFIG']['easyUpdate']) 
+		{
 			if (sizeof($config))
 				$this->Config->update("\$GLOBALS['TL_CONFIG']['easyUpdate']", serialize($config));
 			else
 				$this->Config->delete("\$GLOBALS['TL_CONFIG']['easyUpdate']", 1);
-		} else
+		} 
+		else
 			$this->Config->add("\$GLOBALS['TL_CONFIG']['easyUpdate']", serialize($config));
+		
 		$return .= '</div><div style="float:left; width:40%;">';
 		$return .= '<h2>' . $GLOBALS['TL_LANG']['easyupdate']['noupdate'] . '</h2>';
 		$return .= '<form action="' . ampersand($this->Environment->request) . '" name="tl_select_config" class="tl_form" method="POST">';
@@ -188,15 +224,24 @@ class easyupdate extends BackendModule {
 		$return .= '</div>';
 		return $return;
 	}
-	protected function showChangelog($archive) {
+	
+	/**
+	 * showChangelog
+	 * @param string $archive
+	 * @return string
+	 */
+	protected function showChangelog($archive) 
+	{
 		$archive = 'tl_files/easyupdate/' . (substr($archive, 0, 3) == 'bak' ? 'backup/' : '') . $archive;
 		$objArchive = new ZipReaderTL($archive);
 		$arrFiles = $objArchive->getFileList();
 		$i = strpos($arrFiles[0], '/') + 1;
 		array_shift($arrFiles);
-		while ($objArchive->next()) {
+		while ($objArchive->next()) 
+		{
 			$strfile = substr($objArchive->file_name, $i);
-			if ($strfile == 'system/constants.php') {
+			if ($strfile == 'system/constants.php') 
+			{
 				$constants = ($objArchive->unzip());
 				break;
 			}
@@ -206,11 +251,14 @@ class easyupdate extends BackendModule {
 		$this->IMPORT = $this->getVersionAndBuild($constants);
 		// check the both version
 		$update = $this->checkVersion($this->IMPORT);
-		switch ($update) {
+		switch ($update) 
+		{
 			case 1 :
-				while ($objArchive->next()) {
+				while ($objArchive->next()) 
+				{
 					$strfile = substr($objArchive->file_name, $i);
-					if ($strfile == 'CHANGELOG.txt' && $update != 0) {
+					if ($strfile == 'CHANGELOG.md' && $update != 0) 
+					{
 						$changelog = explode("\n", htmlentities($objArchive->unzip()));
 						break;
 					}
@@ -220,15 +268,18 @@ class easyupdate extends BackendModule {
 				$text = $GLOBALS['TL_LANG']['easyupdate']['changelog']['same'];
 				break;
 			default :
-				$objFile = new File('CHANGELOG.txt');
+				$objFile = new File('CHANGELOG.md');
 				$changelog = explode("\n", htmlentities($objFile->getContent()));
 				$objFile->close();
 				break;
 		}
-		if ($update != 2) {
-			if (sizeof($changelog) > 1) {
+		if ($update != 2) 
+		{
+			if (sizeof($changelog) > 1) 
+			{
 				$pos1 = $pos2 = 0;
-				foreach ($changelog as $i => $text) {
+				foreach ($changelog as $i => $text) 
+				{
 					if (substr_count($text, 'Version') & !$pos2)
 						$pos2 = $i;
 					if (substr_count($text, VERSION . '.' . BUILD))
@@ -240,7 +291,8 @@ class easyupdate extends BackendModule {
 				$m = ($pos1 > $pos2 ? $pos1 : $pos2);
 				for ($i; $i < $m; $i++)
 					$text .= $changelog[$i] . '<br>';
-			} else
+			} 
+			else
 				$text = $GLOBALS['TL_LANG']['easyupdate']['changelog']['no'];
 		}
 		$return .= '<div style="width:700px; margin:0 auto;">';
@@ -254,8 +306,14 @@ class easyupdate extends BackendModule {
 		$return .= '</div></div>';
 		return $return;
 	}
-	// list the files
-	protected function listfiles($archive) {
+
+	/**
+	 * list the files
+	 * @param string $archive
+	 * @return string
+	 */
+	protected function listfiles($archive) 
+	{
 		$archive = 'tl_files/easyupdate/' . (substr($archive, 0, 3) == 'bak' ? 'backup/' : '') . $archive;
 		$objArchive = new ZipReaderTL($archive);
 		$arrFiles = $objArchive->getFileList();
@@ -263,7 +321,8 @@ class easyupdate extends BackendModule {
 		$return .= '<div style="width:700px; margin:0 auto;">';
 		$return .= '<h1 style="font-family:Verdana,sans-serif; font-size:16px; margin:18px 3px;">' . $GLOBALS['TL_LANG']['easyupdate']['content'] . '</h1>';
 		$return .= '<div style="font-family:Verdana,sans-serif; font-size:11px; height:500px; overflow:auto; background:#eee; border:1px solid #999;"><ol style="margin-top:0px">';
-		while ($objArchive->next()) {
+		while ($objArchive->next()) 
+		{
 			$strfile = substr($objArchive->file_name, $i);
 			$return .= '<li>' . $strfile . '</li>';
 		}
@@ -275,8 +334,13 @@ class easyupdate extends BackendModule {
 		$return .= '</div>';
 		return $return;
 	}
-	// backup the current files
-	protected function backupfiles($archive) {
+
+	/**
+	 * backup the current files
+	 * @param string $archive
+	 */
+	protected function backupfiles($archive) 
+	{
 		$archive = 'tl_files/easyupdate/' . (substr($archive, 0, 3) == 'bak' ? 'backup/' : '') . $archive;
 		$objArchive = new ZipReaderTL($archive);
 		$return .= '<div style="width:700px; margin:0 auto;">';
@@ -286,15 +350,21 @@ class easyupdate extends BackendModule {
 		$i = strpos($arrFiles[0], '/') + 1;
 		$objBackup = new ZipWriterTL('tl_files/easyupdate/backup/bak' . date('YmdHi') . '-' . VERSION . '.' . BUILD . '.zip');
 		$rootpath = 'contao-' . VERSION . '.' . BUILD . '/';
-		foreach ($arrFiles as $strFile) {
+		
+		foreach ($arrFiles as $strFile) 
+		{
 			$strFile = substr($strFile, $i);
-			if ($strFile == 'system/runonce.php') {
+			if ($strFile == 'system/runonce.php') 
+			{
 				continue;
 			}
-			try {
+			try 
+			{
 				$objBackup->addFile($strFile, $rootpath . $strFile);
 				$return .= '<li>' . $GLOBALS['TL_LANG']['easyupdate']['backuped'] . $strFile . '</li>';
-			} catch (Exception $e) {
+			} 
+			catch (Exception $e) 
+			{
 				$return .= '<li>' . $GLOBALS['TL_LANG']['easyupdate']['skipped'] . $strFile . ' (' . $e->getMessage() . ')</li>';
 			}
 		}
@@ -307,12 +377,21 @@ class easyupdate extends BackendModule {
 		$return .= '</div>';
 		return $return;
 	}
-	// unzip and copy the files
-	protected function copyfiles($archive) {
+
+	/**
+	 * unzip and copy the files
+	 * @param unknown_type $archive
+	 * @return string
+	 */
+	protected function copyfiles($archive) 
+	{
 		$archive = 'tl_files/easyupdate/' . (substr($archive, 0, 3) == 'bak' ? 'backup/' : '') . $archive;
-		if ($config = unserialize($GLOBALS['TL_CONFIG']['easyUpdate'])) {
-			foreach ($config as $key => $value) {
-				switch ($key) {
+		if ($config = unserialize($GLOBALS['TL_CONFIG']['easyUpdate'])) 
+		{
+			foreach ($config as $key => $value) 
+			{
+				switch ($key) 
+				{
 					case ("demo") :
 						$exclude['basic.css'] = $value;
 						$exclude['print.css'] = $value;
@@ -335,19 +414,24 @@ class easyupdate extends BackendModule {
 		$return .= '<h1 style="font-family:Verdana,sans-serif; font-size:16px; margin:18px 3px;">' . $GLOBALS['TL_LANG']['easyupdate']['update'] . '</h1>';
 		$return .= '<div style="font-family:Verdana,sans-serif; font-size:11px; height:500px; overflow:auto; background:#eee; border:1px solid #999;"><ol style="margin-top:0px">';
 		// Unzip files
-		while ($objArchive->next()) {
+		while ($objArchive->next()) 
+		{
 			$strFile = substr($objArchive->file_name, $i);
-			if ($exclude[$strFile]) {
+			if ($exclude[$strFile]) 
+			{
 				$return .= '<li style="color:#2500ff;">' . $GLOBALS['TL_LANG']['easyupdate']['skipped'] . $strFile . ': ' . $GLOBALS['TL_LANG']['easyupdate']['exclude'] . '</li>';
 				continue;
 			}
-			try {
+			try 
+			{
 				$objFile = new File($strFile);
 				$test = $objArchive->current();
 				$objFile->write($objArchive->unzip());
 				$objFile->close();
 				$return .= '<li>' . $GLOBALS['TL_LANG']['easyupdate']['updated'] . $strFile . '</li>';
-			} catch (Exception $e) {
+			} 
+			catch (Exception $e) 
+			{
 				$return .= '<li style="color:#ff0000;">' . $GLOBALS['TL_LANG']['easyupdate']['error'] . $strFile . ': ' . $e->getMessage() . '</li>';
 			}
 		}
@@ -360,17 +444,22 @@ class easyupdate extends BackendModule {
 		$return .= '</div>';
 		return $return;
 	}
-	//
-	// Function
-	//
-	// get the version and build from the constants.php
-	protected function getVersionAndBuild($temp) {
-		foreach (explode("\n", $temp) as $text) {
-			if (substr_count($text, 'VERSION')) {
+
+	/**
+	 * get the version and build from the constants.php
+	 * @param string $temp
+	 */
+	protected function getVersionAndBuild($temp) 
+	{
+		foreach (explode("\n", $temp) as $text) 
+		{
+			if (substr_count($text, 'VERSION')) 
+			{
 				$pos_v = strpos($text, "'", strpos($text, ",")) + 1;
 				$IMPORT_VERSION = substr($text, $pos_v, strrpos($text, "'") - $pos_v);
 			}
-			if (substr_count($text, 'BUILD')) {
+			if (substr_count($text, 'BUILD')) 
+			{
 				$pos_b = strpos($text, "'", strpos($text, ",")) + 1;
 				$IMPORT_BUILD = substr($text, $pos_b, strrpos($text, "'") - $pos_b);
 			}
@@ -379,33 +468,50 @@ class easyupdate extends BackendModule {
 		$IMPORT['BUILD'] = $pos_b ? $IMPORT_BUILD : 'X';
 		return $IMPORT;
 	}
-	// check if new, same or old version
-	// 	0 => old
-	// 	1 => new 
-	//	2 => same
-	protected function checkVersion($IMPORT) {
+
+	/**
+	 * check if new, same or old version
+	 * @param array $IMPORT
+	 * @return    integer    0 => old
+	 *                       1 => News
+	 *                       2 => same
+	 */
+	protected function checkVersion($IMPORT) 
+	{
 		$BUILD = BUILD;
 		$VERSION = explode(".", VERSION);
 		$VERSION_IMPORT = explode(".", $IMPORT['VERSION']);
 		$BUILD_IMPORT = $IMPORT['BUILD'];
-		if ($VERSION[0] > $VERSION_IMPORT[0]) {
+		if ($VERSION[0] > $VERSION_IMPORT[0]) 
+		{
 			$update = 0;
 		}
-		elseif ($VERSION[0] < $VERSION_IMPORT[0]) {
+		elseif ($VERSION[0] < $VERSION_IMPORT[0]) 
+		{
 			$update = 1;
-		} else {
-			if ($VERSION[1] > $VERSION_IMPORT[1]) {
+		} 
+		else 
+		{
+			if ($VERSION[1] > $VERSION_IMPORT[1]) 
+			{
 				$update = 0;
 			}
-			elseif ($VERSION[1] < $VERSION_IMPORT[1]) {
+			elseif ($VERSION[1] < $VERSION_IMPORT[1]) 
+			{
 				$update = 1;
-			} else {
-				if ($BUILD > $BUILD_IMPORT) {
+			} 
+			else 
+			{
+				if ($BUILD > $BUILD_IMPORT) 
+				{
 					$update = 0;
 				}
-				elseif ($BUILD < $BUILD_IMPORT) {
+				elseif ($BUILD < $BUILD_IMPORT) 
+				{
 					$update = 1;
-				} else {
+				} 
+				else 
+				{
 					$update = 2;
 				}
 			}
